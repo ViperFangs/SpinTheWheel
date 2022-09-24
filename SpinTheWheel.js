69afb225e21de23c127877f3bc88e3715893df9e
Aarya <amathrej@asu.edu>
2022-09-24 13:50:37 -0700

    Initialize FS

--- SpinTheWheel.js
+++ SpinTheWheel.js
@@ -6,3 +6,8 @@ Description: Assignment 1 CPI 310 - Spin The Wheel Game on JavaScript
 
 // Initialize prompt-sync to collect user-prompts
 const PROMPT = require('prompt-sync')();
+const FS = require('fs');
+
+function main() {
+	console.log('Welcome to CPI 310 Fortunate Wheel');
+}
